49723045fbc4ed7dd4251fb27a2b77f928ee0e8a
jhunior <jhuniorcruz@gmail.com>
2025-01-22 13:57:50 -0500

    Primera carga

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:temporizador_app/pagina_general.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            titleTextStyle: TextStyle(fontSize: 28, color: Colors.black),
+            centerTitle: true),
       ),
+      home: PaginaGeneral(),
     );
   }
 }
